47258f9eb4885fad8a0802ef6a6558f5f0e5f197
Megan Frazier <onaccidentproduction@gmail.com>
2020-09-23 14:00:47 -0700

    adding parallax
    use a and d to move the camera now, plus endless scrolling capabilities

--- ProjectMixBoard/Assets/GameScripts/Parallax.cs
+++ ProjectMixBoard/Assets/GameScripts/Parallax.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour {
-
+public class Parallax : MonoBehaviour
+{
+    // Start is called before the first frame update
     private float length, startpos;
     public GameObject cam;
     public float parallaxEffect;
-    void Start() {
+
+    void Start()
+    {
         startpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
-    void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float dist = cam.transform.position.x * parallaxEffect;
 
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
---
+++ ProjectMixBoard/Assets/GameScripts/ToggleOnOFF.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOnOFF : MonoBehaviour
+{
+    public GameObject walkRosa, walkTuck, walkGorn, walkSylric, sittingBodies, sittingHead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        walkGorn.SetActive(false);
+        walkRosa.SetActive(false);
+        walkSylric.SetActive(false);
+        walkTuck.SetActive(false);
+        sittingBodies.SetActive(true);
+        sittingHead.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            walkTuck.SetActive(true);
+            walkGorn.SetActive(true);
+            walkRosa.SetActive(true);
+            walkSylric.SetActive(true);
+            sittingHead.SetActive(false);
+            sittingBodies.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            walkTuck.SetActive(false);
+            walkGorn.SetActive(false);
+            walkRosa.SetActive(false);
+            walkSylric.SetActive(false);
+            sittingBodies.SetActive(true);
+            sittingHead.SetActive(false);
+        }
+    }
+}
