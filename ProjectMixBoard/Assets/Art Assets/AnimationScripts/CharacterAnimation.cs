55e3ebef86882f3d7ee40470844872cff13e7af6
Megan Frazier <onaccidentproduction@gmail.com>
2020-09-23 16:53:38 -0700

    animation updated
    animation scripts updated for idle states

---
+++ ProjectMixBoard/Assets/Art Assets/AnimationScripts/CharacterAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isIdle", false);
+        }
+        else
+        {
+            anim.SetBool("isIdle", true);
+        }
+    }
+}
