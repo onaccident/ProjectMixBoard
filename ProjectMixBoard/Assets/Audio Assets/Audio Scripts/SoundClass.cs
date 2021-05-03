5cd58c8e9fc21201b40decf479b66468a14d50ed
Megan Frazier <onaccidentproduction@gmail.com>
2021-05-03 12:25:36 -0700

    old changes?
    old changes leftover from Riot demo

--- ProjectMixBoard/Assets/Audio Assets/Audio Scripts/SoundClass.cs
+++ ProjectMixBoard/Assets/Audio Assets/Audio Scripts/SoundClass.cs
@@ -9,9 +9,9 @@ public class RandomContainer
     public string name;
     public AudioClip[] clip;
     [Range(0f, 1f)]
-    public float volume;
+    public float volume = 1;
     [Range(.7f, 1.5f)]
-    public float pitch;
+    public float pitch = 1;
     public bool playOnAwake;
     public bool loop;
     public AudioMixerGroup group;
